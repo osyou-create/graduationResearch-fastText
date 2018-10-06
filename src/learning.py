55e92ccae1c83b331203aafb9d4499dc240d7096
osyou-create <hanzo.rl0422@icloud.com>
2018-10-06 16:47:13 +0900

    皮肉文をモデル化

--- src/learning.py
+++ src/learning.py
@@ -1,9 +1,14 @@
 import sys
 import fastText as ft
+import datetime
+
+now = datetime.datetime.today().strftime("%m%d_%H%M")
+
+INPUT_FILE = "hiniku_input01.txt"
+OUTPUT_FILE = f"../bin/model{now}.txt"
 
 argvs = sys.argv
 #input_file = argvs[1]
 
-
-classifier = ft.train_supervised(f"../data/model02.txt")
-classifier.save_model(f'../bin/model02.bin')
+classifier = ft.train_supervised(f"../data/{INPUT_FILE}")
+classifier.save_model(OUTPUT_FILE)

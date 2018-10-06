5d769045485a25120caa45ec876a3265da7776fc
osyou-create <hanzo.rl0422@icloud.com>
2018-10-06 22:24:48 +0900

    テストと検証結果

--- src/learning.py
+++ src/learning.py
@@ -2,13 +2,22 @@ import sys
 import fastText as ft
 import datetime
 
-now = datetime.datetime.today().strftime("%m%d_%H%M")
+INPUT_FILE = "hiniku_input01"
+TEST_FILE = "test"
 
-INPUT_FILE = "hiniku_input01.txt"
-OUTPUT_FILE = f"../bin/model{now}.txt"
+def print_result(N, p, r):
+    print("N\t" + str(N))
+    print("P@{}\t{:.3f}".format(1, p))
+    print("R@{}\t{:.3f}".format(1, r))
 
-argvs = sys.argv
-#input_file = argvs[1]
+def main():
+    train_data = f"../data/{INPUT_FILE}.txt"
+    valid_data = f"../data/{TEST_FILE}.txt"
+    now = datetime.datetime.today().strftime("%m%d_%H%M")
 
-classifier = ft.train_supervised(f"../data/{INPUT_FILE}")
-classifier.save_model(OUTPUT_FILE)
+    model = ft.train_supervised(input=train_data,epoch=1000,loss="hs")
+    print_result(*model.test(valid_data))
+    model.save_model(f"../bin/model{now}.bin")
+
+if __name__ == "__main__":
+    main()
--- src/prediction.py
+++ src/prediction.py
@@ -1,41 +1,32 @@
 import sys
 import fastText as ft
 import MeCab
+import re
+import datetime
 
-class predict:
-    def __init__(self):
-        self.classifier = ft.load_model('../bin/model02.bin')
+now = datetime.datetime.today().strftime("%m%d_%H%M")
 
-    def get_surfaces(self, content):
-        """
-        文書を分かち書き
-        """
-        tagger = MeCab.Tagger('')
-        tagger.parse('')
-        surfaces = []
-        node = tagger.parseToNode(content)
+MODEL_FILE = "../bin/model1006_1925.bin"
+TEST_FILE = "../data/test.txt"
+OUTPUT_FILE = f"../bin/model{now}.csv"
 
-        while node:
-            surfaces.append(node.surface)
-            node = node.next
+def main():
+    classifier = ft.load_model(MODEL_FILE)
+    with open(TEST_FILE, 'r') as fin:
+        qlite = fin.readlines()
 
-        return surfaces
-
-
-    def tweet_class(self, content):
-        """
-        ツイートを解析して分類を行う
-        """
-        words = " ".join(self.get_surfaces(content))
-        print(words)
-        date = self.classifier.predict(text=words,k=3)
-        print(date)
-
+    tagger = MeCab.Tagger('-Owakati')
+    output_csv = []
+    for q in qlite:
+        line = tagger.parse(q)
+        labels, probs = classifier.predict(line.strip(), k=2)
+        output = []
+        for label, prob in zip(labels, probs):
+            output.append(re.sub("__label__", "", label) + ":" + str(prob))
+        output.append(q.strip())
+        output_csv.append(",".join(output))
+    with open(OUTPUT_FILE, "wt") as fout:
+        fout.write("\n".join(output_csv))
 
 if __name__ == '__main__':
-    pre = predict()
-    file = open('../data/la_phone.txt','r')
-    tweet = file.readline()
-    while tweet:
-        pre.tweet_class(tweet)
-        tweet = file.readline()
+    main()
